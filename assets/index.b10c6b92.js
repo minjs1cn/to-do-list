a01c94150d54505b5bd8e97678caa4fb35ae9cda
minjs1cn <75061620+minjs1cn@users.noreply.github.com>
2021-08-18 14:59:31 +0000

    Deploying to gh-pages from  @ 956767990d63043ee1c45fbb83b38ac8107c5660 🚀

--- assets/index.b10c6b92.js
+++
@@ -1,1 +0,0 @@
-import{R as e,T as t}from"./vendor.f41c3f05.js";function r(){return e.createElement(t,null,e.createElement(t.Title,{level:3},"My To Do List"),e.createElement(t.Paragraph,null,"record what you want to do"))}export{r as default};
